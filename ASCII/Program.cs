f15856b86988d8163f6241f7eefbd322580cdf5e
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-06-09 22:47:50 +0100

    Commit All

--- ASCII/Program.cs
+++ ASCII/Program.cs
@@ -22,7 +22,7 @@ namespace ASCII
             {
                 for (int i = 0; i < width; i++)
                 {
-                    Console.Write("#");
+                    Console.Write("█");
                 }
 
                 Console.WriteLine();
@@ -45,13 +45,92 @@ namespace ASCII
                         Console.Write(" ");
                     }
                 }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Checked Pattern");
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    if((i+j)%2 == 0)
+                    {
+                        Console.Write("█");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
 
                 Console.WriteLine();
             }
+            Console.WriteLine();
 
+            Console.WriteLine("Pyramid");
+
+            int pyramidWidth = 2 * height - 1;
+            int centreColumn = height - 1;
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < pyramidWidth; i++)
+                {
+                    if (i >= centreColumn -j && i <= centreColumn + j)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Circle");
+
+            double centerX = width/2.0;
+            double centerY = height/2.0;
+            double radius = 0.0;
+            if(height<width)
+            {
+                radius = 0.5 * height;
+            }
+            else
+            {
+                radius = 0.5 * width;
+            }
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    double dx = i - centerX;
+                    double dy = j - centerY;
+                    double distance = Math.Sqrt(dx * dx + dy * dy);
+
+                    if (distance < radius)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
 
             Console.ReadKey();
-            
             
             
         }
